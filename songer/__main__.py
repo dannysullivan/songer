6e342f7e936a18d8bc2a93d8d133a4580b3eb98c
Daniel Sullivan <danielalexandersullivan@gmail.com>
2015-10-29 18:52:20 -0400

    Start generating lyrics

--- songer/__main__.py
+++ songer/__main__.py
@@ -2,10 +2,38 @@ from song import Song
 import datetime
 import os
 import shutil
+import requests
+from pymarkovchain import MarkovChain
+
+def fetch_lyrics(artist, lines):
+    uri = "http://api.lyricsnmusic.com/songs"
+    params = {
+        'api_key': API_KEY,
+        'artist': artist,
+    }
+    response = requests.get(uri, params=params)
+    lyric_list = response.json()
+
+    lyrics = ''
+    for lyric_dict in lyric_list:
+        lyrics += lyric_dict['snippet'].replace('...', '') + ' '
+
+    # Generate a Markov model
+    mc = MarkovChain('./markov')
+    mc.generateDatabase(lyrics)
+
+    # Add lines of lyrics
+    result = []
+    for line in range(0, lines):
+        line_string = mc.generateString()
+        result.append(line_string)
+    return result
+
 
 def main():
     song = Song()
-    song.add_lyric("take me out to the cliffs", 4)
+    lyric = fetch_lyrics('Coheed and Cambria', 1)[0]
+    song.add_lyric(lyric, 4)
     song.write_to_midi(str(datetime.datetime.now())+".mid")
     # if not os.path.isdir('mp3s'):
         # os.mkdir('mp3s')
